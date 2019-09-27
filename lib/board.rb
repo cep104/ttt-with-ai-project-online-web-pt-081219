af190d241bf7f826373e86884c7a27092cb689c1
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-27 00:37:58 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -4,10 +4,15 @@ class Board
   def initialize 
     reset!
   end
+  
   @cells = []
   
   def reset! 
    self.cells = Array.new(9, " ")
+  end
+  
+  def display 
+    puts @cells
   end
   
 end
